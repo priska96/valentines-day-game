e4be834b8ff012947d38e19bcc235b1b1d262593
priska96 <priskakohnen@gmail.com>
2023-02-12 18:20:07 +0900

    Add files via upload

---
+++ precache-manifest.e485858b03b9d78dad92752bddd1b741.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9df5ce86f9db63776d15b133fee0815",
+    "url": "/valentines-day-game/index.html"
+  },
+  {
+    "revision": "f3f22cab4d69730bcabb",
+    "url": "/valentines-day-game/static/css/main.6a38841c.chunk.css"
+  },
+  {
+    "revision": "a6f167f7496e4867b96c",
+    "url": "/valentines-day-game/static/js/2.17513de0.chunk.js"
+  },
+  {
+    "revision": "334c37f2f68c3b2e5d33ad567fa5aae8",
+    "url": "/valentines-day-game/static/js/2.17513de0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3f22cab4d69730bcabb",
+    "url": "/valentines-day-game/static/js/main.bbfdd588.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/valentines-day-game/static/js/main.bbfdd588.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da8abb7f08637a11fe8c",
+    "url": "/valentines-day-game/static/js/runtime-main.0ba0260d.js"
+  }
+]);
